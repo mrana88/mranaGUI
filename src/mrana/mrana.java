be68d51cdc081dee61b8033685daf8b26906b0aa
RANA <muhammadrana@mail.usf.edu>
2014-08-04 02:49:07 -0400

    branch commit
    GUI branch

--- src/mrana/mrana.java
+++ src/mrana/mrana.java
@@ -4,6 +4,7 @@ public class mrana {
 	public static void main (String args[]){
 		System.out.println("Muhammad Sohel Rana");
 		System.out.println("Making changes");
+		System.out.println("working on GUI branch");
 	}
 
 }
